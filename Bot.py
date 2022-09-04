c5aea1421435fc65a7d700350a74ce9c05afee45
quangvn2508 <quangvn2508@gmail.com>
2022-09-04 10:58:36 +0100

    queue implementation of stalking

--- Bot.py
+++ Bot.py
@@ -11,6 +11,9 @@ NOTIFICATION_CHANNEL = int(os.getenv('NOTIFICATION_CHANNEL'))
 bot = discord.Bot()
 
 leetcodeUsersList = {}
+userList = []
+nextUser = 0
+userCount = 0
 
 @bot.event
 async def on_ready():
@@ -27,34 +30,43 @@ async def add(ctx, username):
         await ctx.respond(f"User **{username}** already been tracked")
         return
 
+    global userCount
     count, error = get_total_question_solved(username)
     if error != None:
         await ctx.respond(f"Unable to get status for **{username}** with error [{error}]")
     else:
         leetcodeUsersList[username] = count
+        userList.append(username)
+        userCount = len(userList)
         saveUser(username)
         await ctx.respond(f'User\'s added, **{username}** solved {count} questions')
 
-@tasks.loop(seconds=5)
+@tasks.loop(seconds=1)
 async def stalk():
     channel = bot.get_channel(NOTIFICATION_CHANNEL)
-    for username, question_count in leetcodeUsersList.items():
-        count, error = get_total_question_solved(username)
-        if error != None:
-            log(f"error [{error}] when stalking user {username}")
-            continue
-        if count == question_count:
-            continue
+    if userCount == 0:
+        return
+    global nextUser
+    username = userList[nextUser]
+    nextUser = (nextUser + 1) % userCount
+    question_count = leetcodeUsersList[username]
 
-        leetcodeUsersList[username] = count
-        if count < question_count:
-            log(f"something wrong, current solved questions ({count}) smaller than server recorded ({question_count}) for user {username}")
-            continue
-        new_quesiton, error =  get_recent_AC_submission(username)
-        if error != None:
-            log(f"error [{error}] when get recent AC for user {username}")
-            continue
-        await channel.send(f"**{username}** just solved **{new_quesiton['title']}** (https://leetcode.com/problems/{new_quesiton['titleSlug']}/) at {timestampToString(new_quesiton['timestamp'])}")
+    count, error = get_total_question_solved(username)
+    if error != None:
+        log(f"error [{error}] when stalking user {username}")
+        return
+    if count == question_count:
+        return
+
+    leetcodeUsersList[username] = count
+    if count < question_count:
+        log(f"something wrong, current solved questions ({count}) smaller than server recorded ({question_count}) for user {username}")
+        return
+    new_quesiton, error =  get_recent_AC_submission(username)
+    if error != None:
+        log(f"error [{error}] when get recent AC for user {username}")
+        return
+    await channel.send(f"**{username}** just solved **{new_quesiton['title']}** (https://leetcode.com/problems/{new_quesiton['titleSlug']}/) at {timestampToString(new_quesiton['timestamp'])}")
 
 if __name__ == "__main__":
     userList = loadUsers()
@@ -64,4 +76,5 @@ if __name__ == "__main__":
             log(f"error [{error}] when get recent AC for user {username}")
             continue
         leetcodeUsersList[username] = count
+    userCount = len(userList)
     bot.run(TOKEN)
